cd63afec33ab964fcc19ac7aa7a0fcac056296e7
bpm19 <bpm19@bpm19s-MacBook-Pro.local>
2020-05-26 17:32:15 -0400

    Correct graphql dependencies + write first mutation test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,35 @@
-import { isValidPassword } from '../src/utils/user';
+import 'cross-fetch/polyfill'
+import ApolloBoost, { gql } from 'apollo-boost'
+import prisma from '../src/prisma'
 
-test('Should reject password shorter than 8 characters', () => {
-    expect(isValidPassword('1234567')).toBe(false);
+const client = new ApolloBoost({
+    uri: 'http://localhost:4000'
 })
 
-test('Should reject password containing the "password" string', () => {
-    expect(isValidPassword('123password345')).toBe(false);
+test('Should create a new user', async () => {
+    const createUser = gql`
+     mutation {
+         createUser(
+             data: {
+                 name: "Benjamin",
+                 email: "benjamin@example.com",
+                 password: "red12345"
+             }
+         ){
+            token,
+            user {
+                id
+            }
+         }
+     }
+    `
+
+    const response = await client.mutate({
+        mutation: createUser
+    })
+
+    console.log(JSON.stringify(response, null, 2))
+    const userExists = await prisma.exists.User({ id: response.data.createUser.user.id })
+
+    expect(userExists).toBe(true)
 })
